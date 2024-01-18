13292b80851e6d3efe7087ac052cdf79b1eae6c0
Lukas <67206451+Gurkenguru@users.noreply.github.com>
2024-01-18 10:00:31 +0000

    update

--- 📈GTAA-Backtesting/import_stock_data.py
+++ 📈GTAA-Backtesting/import_stock_data.py
@@ -4,27 +4,50 @@ import datetime as dt
 from dateutil import relativedelta
 import pandas as pd
 
-database = "/Users/lukas/Library/Mobile Documents/com~apple~CloudDocs/Random_Coding/own_projects/gtaa_backtesting/data/gtaa_database.db"
-try:
-    conn = sql.connect(database)
-    print("Database Sqlite3.db formed.") 
-except:
-    print("Database Sqlite3.db not formed.")
-    
-cur = conn.cursor()
+
+def openConnection():
+    database = "/workspaces/Laufzeitoptimierung/📈GTAA-Backtesting/data/gtaa_database.db"
+    try:
+        conn = sql.connect(database)
+        print("Database Sqlite3.db formed.") 
+    except:
+        print("Database Sqlite3.db not formed.")
+        
+    cur = conn.cursor()
+    return cur, conn
 
 def initialize():
+    """_summary_
+    """
+    cur, conn = openConnection()
     cur.execute("CREATE TABLE Stocks (ticker TEXT PRIMARY KEY, name TEXT NOT NULL)")
     cur.execute("CREATE TABLE Historic (ticker TEXT NOT NULL, date TEXT NOT NULL, quote INT NOT NULL, ttm_sma INT NOT NULL, two_day_sma INT NOT NULL, ten_month_sma INT NOT NULL, six_month_sma INT NOT NULL, five_month_sma INT NOT NULL, three_month_sma INT NOT NULL, two_month_sma INT NOT NULL, PRIMARY KEY (ticker, date), FOREIGN KEY (ticker) REFERENCES Stocks(ticker))")
+    conn.close()
 
 def dropAll():
+    cur, conn = openConnection()
     cur.execute("DROP TABLE Stocks")
     cur.execute("DROP TABLE Historic")
+    conn.close()
 
-dropAll()
-initialize()
+#dropAll()
+#initialize()
 
 def getAverages(ttm, last_200, last_10m, last_6m, last_5m, last_3m, last_2m, quote, quote_date):
+    """get the averages for a stock
+
+    Args:
+        ttm (_type_): trailing twelve months
+        last_200 (_type_): last 200 trading days (about a year)
+        last_10m (_type_): last 10 months
+        last_6m (_type_): last 6 months
+        last_5m (_type_): last 5 months
+        last_3m (_type_): last 3 months
+        last_2m (_type_): last 2 months
+        quote (_type_): quote on quote_date
+        quote_date (_type_): date of quote
+    """
+    cur, conn = openConnection()
     def getTtm(avList):
         last_date = quote_date - relativedelta.relativedelta(years = 1)
         avList.append([quote_date, quote])
@@ -71,10 +94,19 @@ def getAverages(ttm, last_200, last_10m, last_6m, last_5m, last_3m, last_2m, quo
     last_5m = get_x_m(last_5m, 5)
     last_3m = get_x_m(last_3m, 3)
     last_2m = get_x_m(last_2m, 2)
+    
+    
+    conn.close()
     return(ttm, last_200, last_10m, last_6m, last_5m, last_3m, last_2m)
 
 
 def addStockData(ticker):
+    """ only use for new stocks not yet in the database
+    
+    Args:
+        ticker (_type_): ticker whose stock info should be added
+    """
+    cur, conn = openConnection()
     stock_info = yf.Ticker(ticker)
     
     name = stock_info.info["longName"]
@@ -98,16 +130,44 @@ def addStockData(ticker):
         ttm, last_200, last_10m, last_6m, last_5m, last_3m, last_2m = getAverages(ttm, last_200, last_10m, last_6m, last_5m, last_3m, last_2m, quote, quote_date)
         cur.execute("INSERT INTO Historic VALUES (\'" + ticker + "\', \'" + date + "\', \'" + str(quote) + "\', \'" + str(ttm[-1][2]) + "\', \'" + str(last_200[-1][2]) + "\', \'" + str(last_10m[-1][2]) + "\', \'" + str(last_6m[-1][2]) + "\', \'" + str(last_5m[-1][2]) + "\', \'" + str(last_3m[-1][2]) + "\', \'" + str(last_2m[-1][2]) + "\')")
     conn.commit()
+    conn.close()
 
 def updateStockData(ticker):
+    cur, conn = openConnection()
+    """
+    used to update data for listed stock with ticker
+    """
+    def averageSince(avList, num_months, last, price):
+        """_summary_
+        Args:
+            avList (_type_): list of averages for the past num_months months
+            num_months (_type_): number of months the avList is for
+            last (_type_): last (most recent) date in avList
+            price (_type_): price on a given day
+        """
+        latest = []
+        earliest = dt.datetime.now() - relativedelta.relativedelta(months = months)
+        avList = avList[::-1]
+        #find first valid date that should be included in the new avList
+        first_valid = 0
+        while (avList[first_valid][0] - last_date)/dt.timedelta(days = 1) < 0:
+            first_valid += 1
+        avList = avList[first_valid:]
+        trading_days = len(avList)
+        indices = [12, 200, 10, 6, 5, 3, 2]
+        index = indices.index(num_months) + 3
+        avList = avList[first_valid]
+        #new
+
     cur.execute("SELECT date, quote FROM Historic WHERE Historic.ticker = \'" + ticker + "\' ORDER BY date desc")
-    latest = cur.fetchall()
+    data = cur.fetchall()
+    print(data[0])
+    latest = data[0][0]
+    if (dt.datetime.now()-dt.datetime.strptime(latest, '%Y-%m-%d'))/dt.timedelta(days = 1) > 0:
+        pass
+    print(type(latest))
+    conn.close()
     
-    print(str(latest))
     
-    
-addStockData("META")
+#addStockData("META")
 updateStockData("META")
-
-
-conn.close()
