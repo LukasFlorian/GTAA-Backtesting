4b6edc37096aa4fe4f8999115bcd2fa6ff09e107
Gurkenguru <Lukas.richter.1@gmx.net>
2024-03-09 17:45:29 +0100

    commit

--- 📈GTAA-Backtesting/streamlit_app.py
+++ 📈GTAA-Backtesting/streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("GTAA Backtesting Tool")
-st.subheader("By Lukas Richter & Nemanja Cerovac")
+st.subheader("By Lukas Florian Richter & Nemanja Cerovac")
 
 
 st.write("---")
